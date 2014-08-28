39c25e863b18ec5e93e18274c50e93b1097b74ef
Arash Kashi <arashkashi@gmail.com>
2014-08-28 17:17:35 +0200

    added the delegate to the main test view controller

--- Sunflower/TestBaseViewController.swift
+++ Sunflower/TestBaseViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+protocol TestViewControllerDelegate {
+    func onFinishedTesting(word: Word, testType: TestType, testResult: TestResult)
+}
+
 class TestBaseViewController : UIViewController {
     var word : Word?
     var testType: TestType?
+    
+    var delegate: TestViewControllerDelegate?
 }
