bd391b46ef49a41cefb1dbf58c45fabfe5b17bfe
arashkashi@gmail.com <arashkashi@gmail.com>
2014-09-23 23:20:24 +0200

    new script for extracting the words

--- scripts/words.rb
+++ scripts/words.rb
@@ -7,8 +7,16 @@ file = File.open(ARGV[0], 'r')
 file.each_line do |line|
     words = line.split(' ')
     words.each do |word|
+      word = word.gsub(/[;:,\.']/, '')
+      if word
+        word = word.downcase
         hash_of_words[word] = hash_of_words[word] + 1
+      end
     end
 end
+sorted_values = hash_of_words.sort_by { |key, value| -value}
 
-print hash_of_words
+File.open('result.json', 'w') do |f|
+  f.write(sorted_values.to_json)
+end
+
