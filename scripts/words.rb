c0bb4202eea89c0ec79a397a4a82e3db0ef920aa
Arash Kashi <arashkashi@gmail.com>
2014-09-23 18:10:13 +0200

    some progress on the script

--- scripts/words.rb
+++ scripts/words.rb
@@ -1,5 +1,14 @@
+require 'json'
+
+hash_of_words = Hash.new(0)
+
 file = File.open(ARGV[0], 'r')
 
 file.each_line do |line|
-    print line
+    words = line.split(' ')
+    words.each do |word|
+        hash_of_words[word] = hash_of_words[word] + 1
+    end
 end
+
+print hash_of_words
