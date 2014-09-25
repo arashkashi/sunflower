6d87959375db4d6767d6b6d89153fc2bcc43ede4
arashkashi@gmail.com <arashkashi@gmail.com>
2014-09-25 23:14:09 +0200

    more refactoring

--- scripts/sentences.rb
+++ scripts/sentences.rb
@@ -1,40 +1,45 @@
 $LOAD_PATH << '.'
-
 require 'json'
 require 'words'
+require 'minitest/autorun'
+
 
 module Sentence
   include Words
   def Sentence.sentences(filename)
-    hash_of_sentences = Hash.new(0)
-
+    result = []
     file = File.open(filename, 'rb')
 
     file.each_line do |raw_sentence|
-      raw_sentence = raw_sentence.gsub("\n", '')
-      words = Words.wordsFromString(raw_sentence)
-      
-      hash_of_sentences[words.sort] = raw_sentence if words.length > 0
-      break if hash_of_sentences.keys.length > 10
+      result << raw_sentence.gsub("\n", '')
     end
-    
-    hash_of_sentences
+    result
   end
 
-  def sortedSentenceForWord(word, knownWords, sentences)
+  def Sentence.sentenceForWord(word, knownWords, sentences)
     result = Hash.new()
     for sentence in sentences
       words_in_sentence = Words.wordsFromString(sentence)
       known_words_in_sentence = words_in_sentence.select {|word| knownWords.include?(word)}
       result[sentence] = known_words_in_sentence.length.to_f / words_in_sentence.length
     end
-    result.sort_by {|key, value| -value}
+    result = result.sort_by {|key, value| -value}
+    result.first
   end
 end
 
-puts "Aras"
-puts Sentence.sentences(ARGV[0])
+#puts Sentence.sentences(ARGV[0])
 
 #File.open('sentences.json', 'w') do |f|
 #  f.write(hash_of_sentences.to_json)
 #end
+
+words = ['arash', 'kiarash', 'is', 'good', 'boy']
+sentences = []
+sentences << 'arash is good'
+sentences << 'arash is good boy'
+sentences << 'arash and kiarash is good boy'
+
+result = Sentence.sentenceForWord('arash', ['arash'], sentences)
+
+assert_equal('arash is good boy', result, "first case")
