c652e3eeedd0b7f7d257cbe3cea4bd5f15710ac4
arashkashi@gmail.com <arashkashi@gmail.com>
2014-09-25 21:43:56 +0200

    refactored words

--- scripts/words.rb
+++ scripts/words.rb
@@ -5,17 +5,26 @@ module Word
     hash_of_words = Hash.new(0)
     file = File.open(filename, 'r')
     file.each_line do |line|
-      words = line.split(' ')
+      words = Word.wordsFromString(line)
       words.each do |word|
-        word = word.gsub(/[;:,\.']/, '') 
-        if word
-          word = word.downcase
           hash_of_words[word] = hash_of_words[word] + 1
-        end
       end
     end
     file.close
     hash_of_words.sort_by { |key, value| -value}
+  end
+
+  def Word.wordsFromString(input_string)
+    result = []
+    words = input_string.split(' ')
+    words.each do |word|
+      word = word.gsub(/[;:,\.']/, '')
+      if word
+        word = word.downcase
+        result << word
+      end 
+    end
+    result
   end
 end
 
