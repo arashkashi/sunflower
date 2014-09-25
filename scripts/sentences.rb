ef319b28998bba9955482fd779b3d88cfe0cfd84
Arash Kashi <arashkashi@gmail.com>
2014-09-25 16:41:42 +0200

    made the scripts as module

--- scripts/sentences.rb
+++ scripts/sentences.rb
@@ -1,28 +1,32 @@
 require 'json'
 
-hash_of_sentences = Hash.new(0)
+module Sentence
+  def Sentence.sentences(filename)
+    hash_of_sentences = Hash.new(0)
+    file = File.open(filename, 'rb')
 
-file = File.open(ARGV[0], 'rb')
-
-file.each_line do |raw_sentence|
-  words = raw_sentence.split(' ')
-  key = []
-  words.each do |word|
-    if word
-      word = word.downcase
-      key << word
+    file.each_line do |raw_sentence|
+      words = raw_sentence.split(' ')
+      key = []
+      words.each do |word|
+        if word
+          word = word.downcase
+          key << word
+        end
+      end
+  
+      if key.length > 0
+        key = key.sort
+       hash_of_sentences[key] = raw_sentence
+      end
+      break if hash_of_sentences.length > 10
     end
+    hash_of_sentences
   end
-  if key.length > 0
-    key = key.sort
-    hash_of_sentences[key] = raw_sentence
-  end
-
-  break if hash_of_sentences.length > 10
 end
 
-print hash_of_sentences
+puts Sentence.sentences(ARGV[0])
 
-File.open('sentences.json', 'w') do |f|
-  f.write(hash_of_sentences.to_json)
-end
+#File.open('sentences.json', 'w') do |f|
+#  f.write(hash_of_sentences.to_json)
+#end
--- scripts/words.rb
+++ scripts/words.rb
@@ -1,22 +1,27 @@
 require 'json'
 
-hash_of_words = Hash.new(0)
-
-file = File.open(ARGV[0], 'r')
-
-file.each_line do |line|
-    words = line.split(' ')
-    words.each do |word|
-      word = word.gsub(/[;:,\.']/, '')
-      if word
-        word = word.downcase
-        hash_of_words[word] = hash_of_words[word] + 1
+module Word
+  def Word.words(filename)
+    hash_of_words = Hash.new(0)
+    file = File.open(filename, 'r')
+    file.each_line do |line|
+      words = line.split(' ')
+      words.each do |word|
+        word = word.gsub(/[;:,\.']/, '') 
+        if word
+          word = word.downcase
+          hash_of_words[word] = hash_of_words[word] + 1
+        end
       end
     end
+    file.close
+    hash_of_words.sort_by { |key, value| -value}
+  end
 end
-sorted_values = hash_of_words.sort_by { |key, value| -value}
+
+sortedValues = Word.words(ARGV[0])
 
 File.open('words.json', 'w') do |f|
-  f.write(sorted_values.to_json)
+  f.write(sortedValues.to_json)
+  f.close
 end
-
