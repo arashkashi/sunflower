9c4701a87cf2743f58241859570f3c0dffe44e9d
arashkashi@gmail.com <arashkashi@gmail.com>
2014-09-28 11:02:06 +0200

    hash rank should be zero if the word is not in the sentence.

--- scripts/sentenceTest.rb
+++ scripts/sentenceTest.rb
@@ -12,13 +12,24 @@ class TestSentence < Minitest::Test
     end 
     
     def test_1
-      result = Sentence.rankForSentence('arash is very good', ['arash'])
+      result = Sentence.rankForSentence('arash is very good', ['arash'], 'arash')
       assert_equal 0.25, result
     end
 
     def test_2
-      result = Sentence.rankForSentence('arash is very good', ['arash', 'is'])
+      result = Sentence.rankForSentence('arash is very good', ['arash', 'is'], 'arash')
       assert_equal 0.5, result
     end
 
+    def test_3
+      hash_rank = Sentence.rankHash('arash', sentences, ['arash'])
+      assert_equal hash_rank[sentences[1]], 0.25 
+    end
+
+    def test_4
+      hash_rank = Sentence.rankHash('ahmad', sentences, ['arash', 'is'])
+      for sentence in sentences
+        assert_equal hash_rank[sentence], 0
+      end
+    end
 end
--- scripts/sentences.rb
+++ scripts/sentences.rb
@@ -3,27 +3,23 @@ require 'json'
 require 'words'
 
 module Sentence
-  # def Sentence.sentenceForWord(word, knownWords, sentences, n)
-  #   sentence_rank_hash = Hash.new()
-  #   sentences.each do |sentence|
-  #     words_in_sentence = Words.wordsFromString(sentence)
-  #     if words_in_sentence.include?(word)
+  def Sentence.sentenceForWord(word, knownWords, sentences, n)
+    sentence_rank_hash = Sentence.rankHash(word, sentences, knownWords)
+    sorted_sentences_by_ranks = Words.sortedKeysByValue(sentence_rank_hash)
+    return sorted_sentences_by_ranks.take(n)
+  end
 
-  #     end
-  #     if words_in_sentence.length > 0
-  #       known_words_in_sentence = words_in_sentence.select {|w| knownWords.include?(w)}
-  #       result[sentence] = known_words_in_sentence.length.to_f / words_in_sentence.length
-  #     end
-  #   end
-  #   final_result = []
-  #   result.sort_by {|key, value| -value}.map do |pair|
-  #     final_result << pair[0]
-  #   end
-  #   return final_result.take(n)
-  # end
+  def Sentence.rankHash(word, sentences, knownWords)
+    sentence_rank_hash = Hash.new()
+    sentences.each do |sentence|
+      sentence_rank_hash[sentence] = Sentence.rankForSentence(sentence, knownWords, word)
+    end
+    sentence_rank_hash
+  end
 
-  def Sentence.rankForSentence(sentence, knownWords)
+  def Sentence.rankForSentence(sentence, knownWords, word)
     words_in_sentence = Words.wordsFromString(sentence)
+    return 0.0 unless words_in_sentence.include?(word)
     known_words_in_sentence = words_in_sentence.select {|w| knownWords.include?(w)}
     known_words_in_sentence.length.to_f / words_in_sentence.length.to_f
   end
