67d276359bda66bb38bb452080fd6aaade00e0f0
arashkashi@gmail.com <arashkashi@gmail.com>
2014-10-28 10:15:32 +0100

    DD1 . first sentence view controller worked.

--- Sunflower/PresentationViewController.swift
+++ Sunflower/PresentationViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class PresentationViewController : UIViewController {
+class PresentationViewController : UIPageViewController, UIPageViewControllerDataSource {
     
-    @IBOutlet var labelWord: UILabel!
-    @IBOutlet var labelMeaning: UILabel!
+    var word : Word?
     
     var completionHandler: (() -> ())?
-    var word : Word?
 
     func onGotItTapped() {
         self.completionHandler?()
     }
     
     override func viewDidLoad() {
-        self.labelWord.text = self.word!.name
-        self.labelMeaning.text = self.word!.meaning
+        self.dataSource = self
+        
+        self.setViewControllers([self.wordViewcontroller(word!)], direction: .Forward, animated: false) { (success: Bool) -> Void in
+            //
+        }
+    }
+    
+    func wordViewcontroller(word: Word) -> WordPresentationViewController {
+        var wordVC =  WordPresentationViewController(nibName: "WordPresentationViewController", bundle: NSBundle.mainBundle())
+        wordVC.word = word
+        
+        return wordVC
+    }
+    
+    func sentenceViewController(word: Word, sentenceIndex: Int) -> SampleSentenceViewController {
+        var sentenceVC = SampleSentenceViewController(nibName: "SampleSentenceViewController", bundle: NSBundle.mainBundle())
+        sentenceVC.sentence = word.sentences[sentenceIndex]
+        
+        return sentenceVC
+    }
+    
+    // MARK: DataSource
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        return self.sentenceViewController(self.word!, sentenceIndex: 0)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        return self.sentenceViewController(self.word!, sentenceIndex: 0)
     }
 }
---
+++ Sunflower/WordPresentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WordPresentationViewController.swift
+//  Sunflower
+//
+//  Created by Arash K. on 27/10/14.
+//  Copyright (c) 2014 Arash K. All rights reserved.
+//
+
+import UIKit
+
+class WordPresentationViewController: UIViewController {
+    
+    @IBOutlet var labelWord: UILabel!
+    @IBOutlet var labelMeaning: UILabel!
+    
+    var completionHandler: (() -> ())?
+    var word : Word?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.labelWord.text = self.word!.name
+        self.labelMeaning.text = self.word!.meaning
+    }
+}
