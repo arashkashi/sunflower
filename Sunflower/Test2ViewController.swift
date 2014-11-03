84682adf9eb51358c46e1d595707554a6e2150fb
Arash Kashi <arashkashi@gmail.com>
2014-11-03 12:17:55 +0100

    updated the test2 view controller

--- Sunflower/Test2ViewController.swift
+++ Sunflower/Test2ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class Test2ViewController : Test1ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateButtonChoicesWith(self.wordChoices)
+        
+        // Back up the background color
+        self.buttonsBGColor = self.buttonsChoices![0].backgroundColor!
+    }
     
+    override func updateButtonChoicesWith(words: [Word]) {
+        for (index, button) in enumerate(self.buttonsChoices) {
+            button.setTitle(words[index].name, forState: .Normal)
+        }
+    }
+    
+    // MARK: Logic
+    override func checkAnswer(completionHandler:(()->())?) {
+        disableButtons()
+        if selectedAnswer != nil {
+            if selectedAnswer! == word?.name {
+                resultViewController.showCorrect(true
+                    , completionHandler: completionHandler)
+                self.completionHandler?(test!, .Pass, word!)
+            } else {
+                resultViewController.showWrong(true
+                    , completionHandler: completionHandler)
+                self.completionHandler?(test!, .Fail, word!)
+            }
+        }
+    }
 
 
 }
