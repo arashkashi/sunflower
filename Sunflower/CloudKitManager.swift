702433ab5853628d8a141579bd6cc4bbbe6ee6ef
ArashHome <arashHome@ArashWooga.local>
2014-11-13 20:17:05 +0100

    some cloudkit clean up

--- Sunflower/CloudKitManager.swift
+++ Sunflower/CloudKitManager.swift
@@ -12,43 +12,22 @@ import CloudKit
 let kCloudManagerRecordID = "kCloudManagerRecordID"
 
 class CloudKitManager {
-    var userRecordID: CKRecordID? {
-        get {
-            var recordDict = NSUserDefaults.standardUserDefaults().objectForKey(kCloudManagerRecordID) as? Dictionary<String, String>
-            if recordDict != nil {
-                return self.userRecordIDFromDict(recordDict!)
-            } else {
-                self.fetchUserRecordID(nil)
-                return nil
-            }
-        }
-        
-        set {
-            if newValue == nil { self.userRecordID = nil } else {
-                var userRecordIDDict = self.dictFromUserRecordID(newValue!)
-                NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: kCloudManagerRecordID)
-                self.userRecordID = newValue
-            }
-        }
-    }
-    
+
     func fetchUserRecord(handler: (record: CKRecord?, err: NSError!)->() ) {
-        if userRecordID == nil { handler(record: nil, err: NSError(domain: "Missing Record ID", code: 69, userInfo: nil))}
-        
-        CKContainer.defaultContainer().publicCloudDatabase.fetchRecordWithID(userRecordID, completionHandler: { (userRecord: CKRecord!, err: NSError!) -> Void in
-            handler(record: userRecord, err: err)
-        })
-    }
-    
-    func updateUserRecord(userRecord: CKRecord, handler: (CKRecord!, NSError!) -> Void ) {
-        CKContainer.defaultContainer().publicCloudDatabase.saveRecord(userRecord, completionHandler: handler)
+        self.fetchUserRecordID { (recordID, err) -> Void in
+            
+            if recordID == nil { handler(record: nil, err: nil) }
+            
+            CKContainer.defaultContainer().publicCloudDatabase.fetchRecordWithID(recordID, completionHandler: { (record: CKRecord!, error: NSError!) -> Void in
+                    handler(record: record, err: nil)
+            })
+        }
     }
     
     func fetchUserRecordID(handler:((recordID: CKRecordID!, err: NSError!) -> Void)?) {
         CKContainer.defaultContainer().fetchUserRecordIDWithCompletionHandler({ (recordID: CKRecordID!, err: NSError!) -> Void in
             handler?(recordID: recordID, err: err)
             if recordID != nil && err == nil {
-                self.userRecordID = recordID
             } else {
                 // User not authenticated to iCloud
                 if (err.code == 9) {
@@ -58,32 +37,19 @@ class CloudKitManager {
         })
     }
     
+    func saveRecord(record: CKRecord, handler: ((success: Bool)->())? ) {
+        CKContainer.defaultContainer().publicCloudDatabase.saveRecord(record) { (savedRecord: CKRecord!, error: NSError!) -> Void in
+            if error == nil {
+                handler?(success: true)
+            }
+        }
+    }
+    
     // MARK: Initiation
     class var sharedInstance : CloudKitManager {
         struct Static {
             static let instance : CloudKitManager = CloudKitManager()
         }
         return Static.instance
-    }
-    
-    init() {
-    }
-    
-    // MARK: Helper
-    func userRecordIDFromDict(dict: Dictionary<String, String>) -> CKRecordID {
-        var recordName = dict["recordName"]
-        var zoneName = dict["zoneName"]
-        var ownerName = dict["ownerName"]
-        
-        var recordZoneID = CKRecordZoneID(zoneName: zoneName, ownerName: ownerName)
-        return CKRecordID(recordName: recordName, zoneID: recordZoneID)
-    }
-    
-    func dictFromUserRecordID(record: CKRecordID) -> Dictionary<String, String> {
-        var result = Dictionary<String, String>()
-        result["recordName"] = record.recordName
-        result["zoneName"] = record.zoneID.zoneName
-        result["ownerName"] = record.zoneID.ownerName
-        return result
     }
 }
