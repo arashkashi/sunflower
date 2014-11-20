ab1afee28057362c0fa7c6ee430eb197887a7c65
ArashHome <arashHome@ArashWooga.local>
2014-11-20 20:33:24 +0100

    some random stuff before going home

--- Sunflower/AddThreeViewController.swift
+++ Sunflower/AddThreeViewController.swift
@@ -8,22 +8,20 @@
 
 import UIKit
 
-class AddThreeViewController: UIViewController {
+class AddThreeViewController: UIViewController, UITableViewDataSource {
     var tokens: [String]!
     var corpus: String!
     var sourceLanguage: String!
+    var supportedLanagages: [String]
+    
+    var alertViewShown: Bool = false
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        GoogleTranslate.sharedInstance.supportedLanguages { (languages: [Dictionary<String, String>]?, err) -> () in
-//            if err == nil && languages != nil {
-//                self.supportedLanagages = languages!
-//                self.tableView.reloadData()
-//            } else {
-//                self.showErrorAlertWithMesssage("ERR_GOOGLE_API_NETWORD_CONNECTION!")
-//            }
-//        }
+
 
     }
 
@@ -55,7 +53,60 @@ class AddThreeViewController: UIViewController {
 //        
 //        targetLanaguage = cell.detailTextLabel!.text
 //    }
+    // MARK: Logic
+    func updateSupportedLanguages() {
+        GoogleTranslate.sharedInstance.supportedLanguages { (languages: [Dictionary<String, String>]?, err) -> () in
+            if err == nil && languages != nil {
+                self.supportedLanagages = languages!
+                self.tableView.reloadData()
+            } else {
+                self.showErrorAlertWithMesssage("Could not load the supported languages!")
+            }
+        }
+    }
     
+    
+    // MARK: View manipulation
+    func showErrorAlertWithMesssage(message: String) {
+        if alertViewShown { return }
+        
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "OK!", style: .Cancel) { (action) in
+            self.alertViewShown = false
+        }
+        
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true) {
+            self.alertViewShown = true
+        }
+    }
+    
+    func showAllertForMissingInfo(message: String) {
+        var alertController =  UIAlertController(title: "Missing Info", message: message, preferredStyle: .ActionSheet )
+        
+        var retryAction = UIAlertAction(title: "re-try", style: UIAlertActionStyle.Default) { (action: UIAlertAction!) -> Void in
+            
+        }
+        
+        var cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Default) { (action: UIAlertAction!) -> Void in
+            
+        }
+        
+        alertController.addAction(retryAction)
+        
+        self.presentViewController(alertController, animated: true) { () -> Void in
+            //
+        }
+    }
+    
+    // MARK: Table view data source
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+    }
 
 
 
