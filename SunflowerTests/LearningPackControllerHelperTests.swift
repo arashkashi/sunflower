b20836cba9ae633415ef8426186f5cd048739147
arashkashi@gmail.com <arashkashi@gmail.com>
2014-11-22 17:37:31 +0100

    started writing tests for the learning pack controller helper

--- SunflowerTests/LearningPackControllerHelperTests.swift
+++ SunflowerTests/LearningPackControllerHelperTests.swift
@@ -30,9 +30,29 @@ class LearningPackControllerHelperTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testTokensSuccessfullyConvertedToWords() {
+        var tokens = ["token1","token2", "token3", "token4"]
+        var word1 = Word(name: "token1", meaning: "token1", sentences: [])
+        var word2 = Word(name: "token2", meaning: "token2", sentences: [])
+        var result = LearningPackControllerHelper.tokensSuccessfullyConvertedToWords(tokens, words: [word1, word2])
+        
+        XCTAssertTrue(result.includes("token1"), "PASS")
+        XCTAssertTrue(result.includes("token2"), "PASS")
+        XCTAssertFalse(result.includes("token3"), "PASS")
+        XCTAssertFalse(result.includes("token4"), "PASS")
+    }
+    
+    func testTokensFailedConvertedToWords() {
+        var tokens = ["token1","token2", "token3", "token4"]
+        var word1 = Word(name: "token1", meaning: "token1", sentences: [])
+        var word2 = Word(name: "token2", meaning: "token2", sentences: [])
+        
+        var result = LearningPackControllerHelper.tokensFailedConvertedToWords(tokens, words: [word1, word2])
+        
+        XCTAssertFalse(result.includes("token1"), "PASS")
+        XCTAssertFalse(result.includes("token2"), "PASS")
+        XCTAssertTrue(result.includes("token3"), "PASS")
+        XCTAssertTrue(result.includes("token4"), "PASS")
     }
 
     func testPerformanceExample() {
