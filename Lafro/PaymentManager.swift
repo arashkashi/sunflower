d4fc8044a6e881afe77774582a6080eeadbccb15
arashkashi@gmail.com <arashkashi@gmail.com>
2014-11-27 08:24:41 +0100

    added some code to the payment manager

--- Lafro/PaymentManager.swift
+++ Lafro/PaymentManager.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import StoreKit
 
-class PaymentManager {
+class PaymentManager: NSObject,  SKProductsRequestDelegate  {
+    
     
     class var sharedInstance : PaymentManager {
         struct Static {
             static let instance : PaymentManager = PaymentManager()
         }
         return Static.instance
+    }
+    
+    override init() {
+        super.init()
+        self.requestProductsFor(NSSet(array: ["www.sunflower.com.lafro2000laf"]))
+    }
+    
+    func requestProductsFor(productIdentifiers: NSSet) {
+        var request = SKProductsRequest(productIdentifiers: productIdentifiers)
+        request.delegate = self
+        request.start()
+    }
+    
+    
+    // MARK: product Request Delegate
+    func productsRequest(request: SKProductsRequest!,
+        didReceiveResponse response: SKProductsResponse!) {
+        var skProducts = response.products
+            
+            for invalidProducts in response.invalidProductIdentifiers {
+                NSLog("\(invalidProducts)")
+            }
+        
+        for product in skProducts {
+            NSLog("\(product.productIdentifier)+ \(product.localizedTitle) + \(product.price)")
+        }
     }
    
 }
