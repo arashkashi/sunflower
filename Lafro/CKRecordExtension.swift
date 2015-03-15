44b23a18d0f597bc235e0781e0e96777fada1ab5
arashkashi@gmail.com <arashkashi@gmail.com>
2015-03-15 14:14:11 +0100

    extension for record was added.

---
+++ Lafro/CKRecordExtension.swift
@@ -0,0 +1,25 @@
+//
+//  CKRecordExtension.swift
+//  Lafro
+//
+//  Created by Arash K. on 15/03/15.
+//  Copyright (c) 2015 Arash K. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+extension CKRecord {
+    
+    func keysInString() {
+        for key in self.allKeys() {
+            var valueInNumber = self.objectForKey(key as String) as? NSNumber
+            
+            if let value = valueInNumber {
+                println("\(key) -> \(value)")
+            } else {
+                println("\(key) -> 'value not string convertable'")
+            }
+        }
+    }
+}
